97ad7e1c71f01704d00a21d3b4cc78fac2b2d8c3
Zexi Han <zexihan@me.com>
2019-03-02 19:07:49 -0500

    updated the router board

--- src/RouterBoard.js
+++ src/RouterBoard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Landing from './containers/Landing/Landing';
+import Searching from './containers/Searching/Searching';
+import NavBar from './components/NavBar/NavBar';
 
 class RouterBoard extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
 
     }
@@ -14,13 +16,20 @@ class RouterBoard extends Component {
   render() {
     return (
       <div>
-        <h1>TuneS</h1>
         <Router>
-          <Route
-            path="/"
-            exact render={() =>
-            <Landing /> } 
-          />
+          <div>
+            <NavBar />
+            <Route
+              path="/"
+              exact render={props =>
+                <Landing {...props} /> }
+            />
+            <Route
+              path="/search"
+              exact render={props =>
+                <Searching {...props} /> }
+            />
+          </div>
         </Router>
       </div>
     );
