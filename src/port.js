4b3d90ff4acf7e07c91352f58f0390d442f5996a
ZhonghengYang_minemine <mikeyyyang@outlook.com>
2019-03-12 01:18:16 -0400

    bug done

--- src/port.js
+++ src/port.js
@@ -1,3 +1,3 @@
-export default "http://localhost:5009/api"
+export default "https://tune-server.herokuapp.com/api"
 // API_URL = "https://tune-server.herokuapp.com/api"
 //"http://localhost:5009/api"
