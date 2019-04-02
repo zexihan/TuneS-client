6a30baa1fde2030055b059ccca1ba336f5d3acc4
Tao Ouyang <skyezio123@gmail.com>
2019-04-02 19:51:44 -0400

    prepared to push

--- src/views/User.js
+++ src/views/User.js
@@ -9,7 +9,7 @@ import Comments from "../components/User/Comments";
 
 import "../static/views/User.css";
 
-import AuthService from '../services/AuthService';
+import AuthService from "../services/AuthService";
 import ArtistCard from "../components/ArtistCard";
 let authService = AuthService.getInstance();
 
@@ -41,10 +41,12 @@ class User extends Component {
   componentDidMount() {
     authService.getProfile().then(user => {
       if (user.uid !== -1) {
-        if (user.uid == this.props.match.params.id) {
+        if (user.uid === this.props.match.params.id) {
           console.log("myself");
           this.setState({
-            displayName: user.displayName, portrait: user.photo, country: user.country,
+            displayName: user.displayName,
+            portrait: user.photo,
+            country: user.country,
             id: user.uid,
             isLoggedIn: true,
             isMyself: true,
@@ -54,19 +56,19 @@ class User extends Component {
           return null;
         }
       }
-      authService.getPublicProfile(this.props.match.params.id).then(
-        user => {
-          console.log("others");
-          this.setState({
-            displayName: user.displayName, portrait: user.photo, country: user.country,
-            id: user.uid,
-            isLoggedIn: (user.uid !== -1) ? false : true,
-            comments: user.comments,
-            commentLikes: user.commentLikes
-          });
-        }
-      );
-    })
+      authService.getPublicProfile(this.props.match.params.id).then(user => {
+        console.log("others");
+        this.setState({
+          displayName: user.displayName,
+          portrait: user.photo,
+          country: user.country,
+          id: user.uid,
+          isLoggedIn: user.uid !== -1 ? false : true,
+          comments: user.comments,
+          commentLikes: user.commentLikes
+        });
+      });
+    });
   }
 
   followHandler = () => {
@@ -78,25 +80,27 @@ class User extends Component {
   };
 
   render() {
-
     const recentComments = this.state.comments.map(comment => {
       return (
         <li key={comment._id}>
           <Link to={"/" + comment.subjectType + "/" + comment.subjectId}>
             {comment.content.slice(0, 20)}
           </Link>
-        </li>);
-    })
+        </li>
+      );
+    });
 
     const favoriteComments = this.state.commentLikes.map(commentLike => {
       return (
         <li key={commentLike._id}>
-          <Link to={"/" + commentLike.subjectType + "/" + commentLike.subjectId}>
+          <Link
+            to={"/" + commentLike.subjectType + "/" + commentLike.subjectId}
+          >
             {commentLike.content.slice(0, 20)}
           </Link>
         </li>
-      )
-    })
+      );
+    });
 
     const favoriteSubjects = this.state.subjectLikes.map(subjectLike => {
       return (
@@ -106,7 +110,7 @@ class User extends Component {
           {/* {subjectType === "album" && <AlbumCard album={subjectLike.album} />} */}
         </li>
       );
-    })
+    });
 
     return (
       <div className="container">
@@ -114,30 +118,45 @@ class User extends Component {
           <div className="col">
             <div className="row">
               <div className="col-auto profile-image">
-                <img className="img-fluid" src={this.state.portrait === "" ?
-                  "https://northmemorial.com/wp-content/uploads/2016/10/PersonPlaceholder.png" : this.state.portrait} />
+                <img
+                  className="img-fluid"
+                  src={
+                    this.state.portrait === ""
+                      ? "https://northmemorial.com/wp-content/uploads/2016/10/PersonPlaceholder.png"
+                      : this.state.portrait
+                  }
+                  alt="img"
+                />
               </div>
               <div className="col">
                 <div className="row">
                   <div className="col">
-                    <h2 className="displayName my-1">{this.state.displayName}</h2>
+                    <h2 className="displayName my-1">
+                      {this.state.displayName}
+                    </h2>
                     <div className="bio my-1">bio</div>
-                    <div className="location my-1"><i className="fas fa-map-marker-alt"></i> {this.state.country}</div>
-                    <div className="follow-count my-1">Following: 0 Followers: 0</div>
+                    <div className="location my-1">
+                      <i className="fas fa-map-marker-alt" />{" "}
+                      {this.state.country}
+                    </div>
+                    <div className="follow-count my-1">
+                      Following: 0 Followers: 0
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div className="row my-3">
-              {this.state.isMyself === false && this.state.isLoggedIn === true ? (
+              {this.state.isMyself === false &&
+              this.state.isLoggedIn === true ? (
                 <div className="col-auto">
                   <button
                     type="button"
                     className="btn btn-light mt-2"
                     onClick={this.followHandler}
                   >
-                    <i className="fas fa-plus"></i> Follow
-                </button>
+                    <i className="fas fa-plus" /> Follow
+                  </button>
                 </div>
               ) : null}
               {this.state.isMyself === true ? (
@@ -147,7 +166,7 @@ class User extends Component {
                     className="btn btn-light  mt-2"
                     onClick={this.editProfileHandler}
                   >
-                    <i className="fas fa-user-edit"></i> Edit Profile
+                    <i className="fas fa-user-edit" /> Edit Profile
                   </Link>
                 </div>
               ) : null}
@@ -163,9 +182,7 @@ class User extends Component {
           </div>
           <hr className="user-hr" />
           <div className="row mt-1 ml-2">
-            <ul>
-              {recentComments}
-            </ul>
+            <ul>{recentComments}</ul>
           </div>
         </div>
 
@@ -177,9 +194,7 @@ class User extends Component {
           </div>
           <hr className="user-hr" />
           <div className="row mt-1 ml-2">
-            <ul>
-              {favoriteSubjects}
-            </ul>
+            <ul>{favoriteSubjects}</ul>
           </div>
         </div>
 
@@ -191,9 +206,7 @@ class User extends Component {
           </div>
           <hr className="user-hr" />
           <div className="row mt-1 ml-2">
-            <ul>
-              {favoriteComments}
-            </ul>
+            <ul>{favoriteComments}</ul>
           </div>
         </div>
 
@@ -236,7 +249,7 @@ class User extends Component {
             <li className="row playlist-song">
               <div className="col-6 playlist-song-name">
                 How to Save a Life, The Fray
-                  </div>
+              </div>
               <div className="col-6 playlist-song-tools py-1">
                 <i className="far fa-trash-alt mx-2 color-tomato float-right" />
                 <i className="fas fa-redo-alt mx-2 color-tomato float-right" />
@@ -244,9 +257,7 @@ class User extends Component {
               </div>
             </li>
             <li className="row playlist-song">
-              <div className="col-6 playlist-song-name">
-                Boston, Augustana
-                  </div>
+              <div className="col-6 playlist-song-name">Boston, Augustana</div>
               <div className="col-6 playlist-song-tools py-1">
                 <i className="far fa-trash-alt mx-2 color-tomato float-right" />
                 <i className="fas fa-redo-alt mx-2 color-tomato float-right" />
@@ -255,7 +266,6 @@ class User extends Component {
             </li>
           </ul>
         </div>
-
       </div>
     );
   }
