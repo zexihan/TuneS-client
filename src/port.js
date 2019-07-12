46062159e81c27a29660a370d66a3e15c33816a0
Zexi Han <zexihan@me.com>
2019-07-11 19:23:01 -0700

    updated server addr

--- src/port.js
+++ src/port.js
@@ -1,4 +1,4 @@
-export default "https://tune-server.herokuapp.com";
+export default "https://tunes-api.herokuapp.com";
 // "https://tune-server.herokuapp.com";
 // "https://tunes-api.herokuapp.com";
 // "http://localhost:5000"
